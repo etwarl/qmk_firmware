aeed5af96871ec80b64af8fe33188c9372edaaa1
etwarl <takuryu.t@gmail.com>
2018-07-23 10:37:54 +0900

    Fix cant adjust

--- keyboards/helix/rev2/keymaps/etwarl/keymap.c
+++ keyboards/helix/rev2/keymaps/etwarl/keymap.c
@@ -70,8 +70,8 @@ enum { //tapdance_keycodes
 #define KC_COPY LCTL(KC_C)
 #define KC_PST  LCTL(KC_V)
 #define KC_CAD  LCTL(LALT(KC_DEL))
-#define KC_LWMH LT(_LOWER,KC_MHEN)
-#define KC_RIHE LT(_RAISE,KC_HENK)
+#define KC_LWEI LT(_LOWER,EISU)
+#define KC_RIKN LT(_RAISE,KANA)
 #define KC_CTSP LCTL_T(KC_SPC)
 
 //Macros
@@ -89,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
    * | Sft  |   Z  |   X  |   C  |   V  |   B  | ([{  | )]}  |   N  |   M  |  ,<  |  .>  |  Up  |  /?  |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |  `~  | KANA | GUI  | Alt  |LowMhe|CtrSp |Space |  BS  |Enter |RaiHen|  =+  | Left | Down |Right |
+   * |  `~  |Adjust| GUI  | Alt  |LowEI |CtrSp |Space |  BS  |Enter |RaiKN |  =+  | Left | Down |Right |
    * `-------------------------------------------------------------------------------------------------'
    */ 
   [_QWERTY] = LAYOUT( \
@@ -97,7 +97,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,\
       KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,\
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    TD(LBR), TD(RBR), KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_SLSH,\
-      KC_GRV,  KANA,    WN_LGUI, WN_LALT, KC_LWMH, KC_CTSP, KC_SPC,  KC_BSPC, KC_ENT,  KC_RIHE, KC_EQL,  KC_LEFT, KC_DOWN, KC_RGHT\
+      KC_GRV,  ADJUST,  WN_LGUI, WN_LALT, KC_LWMH, KC_CTSP, KC_SPC,  KC_BSPC, KC_ENT,  KC_RIHE, KC_EQL,  KC_LEFT, KC_DOWN, KC_RGHT\
       ),
 
   /* Eucalyn
