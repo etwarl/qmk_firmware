3b541dd50bea61962e05443ca53231f4c2dc8bae
etwarl <takuryu.t@gmail.com>
2018-07-23 12:49:03 +0900

    edit etwarl keymap

--- keyboards/helix/rev2/keymaps/etwarl/keymap.c
+++ keyboards/helix/rev2/keymaps/etwarl/keymap.c
@@ -70,8 +70,6 @@ enum { //tapdance_keycodes
 #define KC_COPY LCTL(KC_C)
 #define KC_PST  LCTL(KC_V)
 #define KC_CAD  LCTL(LALT(KC_DEL))
-#define KC_LWEI LT(_LOWER,EISU)
-#define KC_RIKN LT(_RAISE,KANA)
 #define KC_CTSP LCTL_T(KC_SPC)
 
 //Macros
@@ -97,7 +95,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,\
       KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,\
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    TD(LBR), TD(RBR), KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_SLSH,\
-      KC_GRV,  ADJUST,  WN_LGUI, WN_LALT, KC_LWMH, KC_CTSP, KC_SPC,  KC_BSPC, KC_ENT,  KC_RIHE, KC_EQL,  KC_LEFT, KC_DOWN, KC_RGHT\
+      KC_GRV,  ADJUST,  WN_LGUI, WN_LALT, LOWER,   KC_CTSP, KC_SPC,  KC_BSPC, KC_ENT,  RAISE,   KC_EQL,  KC_LEFT, KC_DOWN, KC_RGHT\
       ),
 
   /* Eucalyn
@@ -118,7 +116,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,\
       KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,\
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    TD(LBR), TD(RBR), KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_SLSH,\
-      KC_GRV,  KANA,    WN_LGUI, WN_LALT, KC_LWMH, KC_CTSP, KC_SPC,  KC_BSPC, KC_ENT,  KC_RIHE, KC_EQL,  KC_LEFT, KC_DOWN, KC_RGHT\
+      KC_GRV,  KANA,    WN_LGUI, WN_LALT, LOWER, KC_CTSP, KC_SPC,  KC_BSPC, KC_ENT,  RAISE, KC_EQL,  KC_LEFT, KC_DOWN, KC_RGHT\
       ),
 
   /* Lower
