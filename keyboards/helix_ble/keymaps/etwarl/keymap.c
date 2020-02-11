7cb4956a17f7ba709130f02c18b6aac8178047a1
etwarl <takuryu.t@gmail.com>
2020-02-12 02:30:28 +0900

    update helix-ble keymap

--- keyboards/helix_ble/keymaps/etwarl/keymap.c
+++ keyboards/helix_ble/keymaps/etwarl/keymap.c
@@ -102,7 +102,7 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,\
       KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,\
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    TD(LBR), TD(RBR), KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_EQL,\
-      KC_GRV,  KC_LGUI, KC_LALT, KC_ZNHN, LOWER,   KC_CTSP, KC_SPC,  KC_BSPC, KC_ENT,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT\
+      KC_GRV,  KC_LGUI, KC_LALT, KC_ZNHN, KC_LSFT,   KC_CTSP, KC_SPC,  KC_BSPC, KC_ENT,  KC_RSFT,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT\
       ),
 
   /* Eucalyn
